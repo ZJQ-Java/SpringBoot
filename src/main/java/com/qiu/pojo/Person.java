e57545660ba91e450659b8dbe1b107a4a7d02df9
zhangjinqiu03 <zhangjinqiu03@58.com>
2021-03-15 19:56:09 +0800

    start

---
+++ src/main/java/com/qiu/pojo/Person.java
@@ -0,0 +1,84 @@
+package com.qiu.pojo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Email;
+import java.util.List;
+import java.util.Map;
+
+@Component
+@ConfigurationProperties(prefix = "person")
+//@PropertySource(value = "classpath:user.properties")
+@Validated
+public class Person {
+
+    private String              name;
+    private Integer             age;
+    @Email
+    private String              email;
+    private List<Object>        lists;
+    private Map<String, String> maps;
+    private Dog                 dog;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                ", lists=" + lists +
+                ", maps=" + maps +
+                ", dog=" + dog +
+                '}';
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public List<Object> getLists() {
+        return lists;
+    }
+
+    public void setLists(List<Object> lists) {
+        this.lists = lists;
+    }
+
+    public Map<String, String> getMaps() {
+        return maps;
+    }
+
+    public void setMaps(Map<String, String> maps) {
+        this.maps = maps;
+    }
+
+    public Dog getDog() {
+        return dog;
+    }
+
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
+}
