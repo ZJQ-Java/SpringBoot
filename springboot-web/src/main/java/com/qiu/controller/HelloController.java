f01a4f82cf50cb5474bafa7d8a8f5534c59b8862
zhangjinqiu03 <zhangjinqiu03@58.com>
2021-04-13 16:15:35 +0800

    restpost请求转put,delete

--- springboot-web/src/main/java/com/qiu/controller/HelloController.java
+++ springboot-web/src/main/java/com/qiu/controller/HelloController.java
@@ -4,18 +4,32 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Arrays;
 
 @RestController
 public class HelloController {
-    @RequestMapping("/hello")
-    public String thymeleaf(Model model){
-        model.addAttribute("name","<h1> hello springboot thymeleaf </h1>");
-        model.addAttribute("users", Arrays.asList("zjq","哈哈"));
-        return "hello";
+    @RequestMapping(value = "/user",method = RequestMethod.GET)
+    public String getUser(){
+        return "GET-张三";
     }
+
+    @RequestMapping(value = "/user",method = RequestMethod.POST)
+    public String saveUser(){
+        return "POST-张三";
+    }
+
+
+    @RequestMapping(value = "/user",method = RequestMethod.PUT)
+    public String putUser(){
+        return "PUT-张三";
+    }
+
+    @RequestMapping(value = "/user",method = RequestMethod.DELETE)
+    public String deleteUser(){
+        return "DELETE-张三";
+    }
+
 
 }
